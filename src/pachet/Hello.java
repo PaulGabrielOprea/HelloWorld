38c9850dd79047611c0b7a74484552160dfb7f34
Axinte Ioana <ioana.axinte@student.tuiasi.ro>
2022-03-06 11:16:27 +0200

    Am adaugat functia suma

--- src/pachet/Hello.java
+++ src/pachet/Hello.java
@@ -1,13 +1,18 @@
 package pachet;
 
-
 public class Hello {
+	
+	static int suma(int a, int b){
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		
 		System.out.println("Ioana a scris aici");
+		
+		System.out.println("suma este " + suma(5,7));
 		
 	}
 
