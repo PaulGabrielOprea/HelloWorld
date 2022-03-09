3c1b70865c6657f499d8bdc9e104c99c9fd4e621
Mihaela Cebotar <vacarasu.flavia@yahoo.com>
2022-03-09 10:48:19 +0200

    Adaugare fisier

---
+++ src/pachet/Hello_1.java
@@ -0,0 +1,21 @@
+package pachet;
+
+public class Hello_1 {
+	
+	static int suma(int a, int b){
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		
+		System.out.println("Ioana a scris aici");
+		
+		System.out.println("suma este " + suma(5,7));
+		
+		System.out.println("Mesaj branch master");
+
+	}
+
+}
